25da2f22a64169ae445669472131452a0c689096
Peter Hosey <git@boredzo.org>
2013-02-28 17:27:05 -0800

    Initial Commit

---
+++ PNG DPI/PRHDocument.h
@@ -0,0 +1,3 @@
+@interface PRHDocument : NSDocument
+
+@end
---
+++ PNG DPI/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PNG DPI
+//
+//  Created by Peter Hosey on 2013-02-28.
+//  Copyright (c) 2013 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
